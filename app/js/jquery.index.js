7f3dacf7e9216658bfc452ec27912092a5a97183
mbomak <mboma@meta.ua>
2017-01-16 20:36:54 +0200

    finish

--- app/js/jquery.index.js
+++ app/js/jquery.index.js
@@ -1,3 +1,50 @@
-( function(){
+"use strict";
+( function () {
 
-} )();
+  $( function() {
+
+    $.each( $( '.show-psw' ), function() {
+       
+      new  Show ( $( this ) );
+       
+    } );
+
+  } );
+
+    
+  var Show = function ( obj ) {
+  
+    //private properties
+    var _checkbox = obj,
+    		_password = $('.site__main-password');
+
+    //private methods
+    var _constructor = function () {
+
+            _onEvents();
+    
+        },
+    _onEvents = function () {
+    
+        _checkbox = obj.on ( {
+            change: function () {
+             
+             _toggleMenu();
+
+            }
+        } );
+ 
+    },
+  
+    _toggleMenu = function () {
+    	console.log(_password);
+
+    	_password.attr('type') = 'text';
+  	  	
+    };
+  
+    _constructor ();
+  
+  }
+ 
+} ) ();
