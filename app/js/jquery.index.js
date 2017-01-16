160ba45198101788646da77cee7c254df47eb0f8
Sergey <kuzmenkos1986@gmail.com>
2017-01-16 18:56:45 +0200

    step-9 finish header

--- app/js/jquery.index.js
+++ app/js/jquery.index.js
@@ -1,3 +1,82 @@
 ( function(){
 
+    "use strict";
+
+    $( function(){
+
+        $.each( $( '.menu-btn' ), function() {
+            new Menu ( $( this ) );
+        } );
+
+    } );
+
+    var Menu = function( obj ) {
+
+        //private properties
+        var _obj = obj,
+            _menu = $( '.menu' ),
+            _window = $( window );
+
+
+        //private methods
+        var _initSlider = function() {
+
+                _obj.on( {
+                    'click': function() {
+
+                        if ( _obj.hasClass( 'close' ) ) {
+
+                            _obj.removeClass( 'close' );
+                            _menu.removeClass( 'opened' );
+
+                        } else {
+
+                            _obj.addClass( 'close' );
+                            _menu.addClass( 'opened' );
+
+                            if ( _menu.height() - 10 > _window.height() ) {
+                                _initContentScroll();
+                                $( _menu ).getNiceScroll().show();
+                            } else {
+                                $( _menu ).getNiceScroll().hide();
+                            }
+
+                        }
+                        return false;
+                    }
+                } );
+                /*_window.on( {
+                    'resize': function() {
+
+                        if ( _menu.height() - 10 > _window.height() && _obj.hasClass( 'menu-btn_close' ) ) {
+                            _initContentScroll();
+                            $( _menu ).getNiceScroll().show();
+                        } else {
+                            $( _menu ).getNiceScroll().hide();
+                        }
+
+                    }
+                } )*/
+
+            },
+            _initContentScroll = function() {
+                $( _menu ).niceScroll( {
+                    autohidemode: 'false',
+                    cursorborder: '',
+                    cursorcolor: "#fff",
+                    cursorwidth: "6px",
+                    cursorborderradius: "0"
+                } );
+            },
+            _init = function() {
+                _initSlider();
+            };
+
+        //public properties
+
+        //public methods
+
+        _init();
+    };
+
 } )();
