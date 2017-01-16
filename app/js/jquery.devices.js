fe034d41b615846ef61c2d61d80d58b512453521
mbomak <mboma@meta.ua>
2017-01-16 16:02:12 +0200

    step2-3 merge

--- app/js/jquery.devices.js
+++ app/js/jquery.devices.js
@@ -8,12 +8,6 @@
 
         });
 
-        $( '.defaults' ).each(function(){
-
-            new Toggle( $(this) );
-
-        });
-
     });
 
     var AddScroll = function( obj ){
@@ -40,52 +34,6 @@
 
         _init();
     };
-
-    var Toggle = function ( obj ) {
-    
-      //private properties
-      var _container = obj,
-          _btn = $( '.defaults__title' ),
-          _wrap1 = $( '.defaults__wrap-1' ),
-          _wrap2 = $( '.defaults__wrap-2' );
-
-      //private methods
-      var _constructor = function () {
-
-              _onEvents();
-      
-          },
-      _onEvents = function () {
-      
-          _btn.on ( {
-              click: function () {
-
-               _toggleMenu();
-
-              }
-          } );  
-      },
-    
-      _toggleMenu = function () {
-
-        if ( _btn.hasClass( 'defaults__title-1' ) ) {
-
-            _wrap1.slideToggle();
-
-            _btn.toggleClass('defaults__title_open');
-
-        } else if ( _btn.hasClass( 'defaults__title-2' ) ) {
-
-            _wrap2.slideToggle();
-
-        }
-
-      };
-    
-      _constructor ();
-    
-    };
-
 
 } )();
 
